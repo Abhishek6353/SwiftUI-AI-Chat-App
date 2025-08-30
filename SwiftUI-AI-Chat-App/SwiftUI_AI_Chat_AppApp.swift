f0f6d00cbd6d3de767faa59aebcd151f8675ba5f
Abhishek Kurmi <itsabhi814@gmail.com>
2025-08-30 15:50:30 +0530

    Initial Commit

---
+++ SwiftUI-AI-Chat-App/SwiftUI_AI_Chat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AI_Chat_AppApp.swift
+//  SwiftUI-AI-Chat-App
+//
+//  Created by Apple on 30/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AI_Chat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
