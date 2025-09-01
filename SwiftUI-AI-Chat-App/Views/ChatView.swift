8a5e15dead475c315d39163ad2b3b6c4943fada4
Abhishek Kurmi <itsabhi814@gmail.com>
2025-09-01 18:15:30 +0530

    - created chatview

---
+++ SwiftUI-AI-Chat-App/Views/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  SwiftUI-AI-Chat-App
+//
+//  Created by Apple on 01/09/25.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}
