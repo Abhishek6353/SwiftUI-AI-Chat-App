91bba12f482735bfda62eb3a9da24174cda653ec
Abhishek Kurmi <itsabhi814@gmail.com>
2025-09-01 16:21:12 +0530

    - design headerview in home screen

--- SwiftUI-AI-Chat-App/Views/HomeView.swift
+++ SwiftUI-AI-Chat-App/Views/HomeView.swift
@@ -10,13 +10,51 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack(spacing: 10) {
+                ZStack {
+                    Circle()
+                        .fill(.primaryOrange)
+                        .frame(width: 42, height: 42)
+                    
+                    Image("placeholderUser")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .clipShape(.circle)
+                }
+                
+                HStack(spacing: 0) {
+                    Text("Hello, ")
+                        .font(Font.system(size: 18))
+                        .foregroundStyle(.primaryText)
+                    
+                    Text("Arthur!")
+                        .fontWeight(.bold)
+                        .font(Font.system(size: 18))
+                        .foregroundStyle(.primaryText)
+                }
+                Spacer()
+                
+                Button(action: {
+                    /// Action here
+                }) {
+                    Image(systemName: "line.3.horizontal")
+                        .tint(.primaryWhite)
+                        .imageScale(.large)
+                        .frame(width: 42, height: 42)
+                }
+                
+            }
+            .frame(height: 42)
+            .padding(.horizontal, 20)
+            
+            Spacer()
+            
         }
-        .padding()
+        .padding(.top, 10)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.primaryBackground)
     }
+    
 }
 
 #Preview {
