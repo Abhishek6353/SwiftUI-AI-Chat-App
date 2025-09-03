089974d3d54f88cf39ae3656dfbe78f39fcd49f6
Abhishek Kurmi <itsabhi814@gmail.com>
2025-09-03 12:04:53 +0530

    Created Helper
    - created struct Config inside Helper.swift which will provide Gemini API key from Config.plisy

---
+++ SwiftUI-AI-Chat-App/Utility/Helper.swift
@@ -0,0 +1,19 @@
+//
+//  Helper.swift
+//  SwiftUI-AI-Chat-App
+//
+//  Created by Apple on 03/09/25.
+//
+
+import Foundation
+
+struct Config {
+    static var geminiApiKey: String {
+        if let path = Bundle.main.path(forResource: "Config", ofType: "plist"),
+           let dict = NSDictionary(contentsOfFile: path) as? [String: Any],
+           let key = dict["GEMINI_API_KEY"] as? String {
+            return key
+        }
+        fatalError("⚠️ GEMINI_API_KEY not set. Please create Config.plist from Config.plist.example")
+    }
+}
